97638993e969c64ea60568b1ec5aab39ab2fcf3e
sheetal <sheetal.singh458@gmail.com>
2021-01-05 12:35:57 +0530

    update simulator

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,269 +1,5 @@
 document.getElementById("container").style.display = "none";
 document.getElementById("container_task").style.display = "none";
-// document.getElementById("task").disabled = true;
-// document.getElementById("machine_image").src = "./images/001.png";
-// document.getElementById("magSection").style.display = "none";
-// document.getElementById("spotSection").style.display = "none";
-// document.getElementById("voltage").style.display = "none";
-
-// function start() {
-//     document.getElementById("container").style.display = "block";
-//     document.getElementById("start").style.display = "none";
-//     textToSpeech("select the material");
-//     document.getElementById("execute_button").disabled = true;
-//     var i = 0;
-//     var txt = '1. Select the Material.';
-//     var speed = 50;
-//     typeWriter();
-
-//     function typeWriter() {
-//         if (i < txt.length) {
-//             document.getElementById("demo1").innerHTML += txt.charAt(i);
-//             i++;
-//             setTimeout(typeWriter, speed);
-//         }
-//     }
-// }
-
-// function display(val) {
-//     if (val == 1) {
-//         document.getElementById("voltage").style.display = "block";
-//         document.getElementById("voltage_btn"), disabled = true;
-//         document.getElementById("magnification_btn").disabled = true;
-//         document.getElementById("spotsize_btn").disabled = true;
-//         document.getElementById("execute_button").disabled = false;
-//         document.getElementById("voltage_btn").style.backgroundColor = "#2EBDC1";
-//     } else if (val == 2) {
-//         document.getElementById("voltage_btn"), disabled = true;
-//         document.getElementById("magnification_btn").disabled = true;
-//         document.getElementById("spotsize_btn").disabled = true;
-//         document.getElementById("magSection").style.display = "block";
-//         document.getElementById("execute_button").disabled = false;
-//         document.getElementById("magnification_btn").style.backgroundColor = "#2EBDC1";
-//     } else if (val == 3) {
-//         document.getElementById("spotSection").style.display = "block";
-//         document.getElementById("voltage_btn"), disabled = true;
-//         document.getElementById("magnification_btn").disabled = true;
-//         document.getElementById("spotsize_btn").disabled = true;
-//         document.getElementById("execute_button").disabled = false;
-//         document.getElementById("spotsize_btn").style.backgroundColor = "#2EBDC1";
-//     }
-//     c = val;
-// }
-// var img;
-// var c;
-// document.getElementById("select2").style.display = "none";
-
-// function clickbtn(val) {
-//     document.getElementById("machine_image").src = "./gif/material1.gif";
-//     document.getElementById("demo").innerHTML = "0 kilo Volts";
-//     document.getElementById("btn1").disabled = true;
-//     document.getElementById("btn2").disabled = true;
-//     document.getElementById("btn3").disabled = true;
-//     document.getElementById("select2").style.display = "block";
-
-//     var i = 0;
-//     var txt = '2. Select the type of experiment to be performed.';
-//     var speed = 50;
-//     typeWriter();
-
-//     function typeWriter() {
-//         if (i < txt.length) {
-//             document.getElementById("demo2").innerHTML += txt.charAt(i);
-//             i++;
-//             setTimeout(typeWriter, speed);
-//         }
-//     }
-
-//     console.log(c);
-//     var d = document.getElementById("myRange");
-//     img = document.createElement('img');
-//     // Update the current d value (each time you drag the d handle)
-//     d.onchange = function() {
-//         var i = 0;
-//         var txt = '3. Click on the "Execute" button';
-//         var speed = 50;
-//         typeWriter();
-//         textToSpeech("click on the execute button");
-
-//         function typeWriter() {
-//             if (i < txt.length) {
-//                 document.getElementById("demo3").innerHTML += txt.charAt(i);
-//                 i++;
-//                 setTimeout(typeWriter, speed);
-//             }
-//         }
-//     }
-//     document.getElementById("myMagnification").onchange = function() {
-//             var i = 0;
-//             var txt = '3. Click on the "Execute" button';
-//             var speed = 50;
-//             typeWriter();
-//             textToSpeech("click on the execute button");
-
-//             function typeWriter() {
-//                 if (i < txt.length) {
-//                     document.getElementById("demo3").innerHTML += txt.charAt(i);
-//                     i++;
-//                     setTimeout(typeWriter, speed);
-//                 }
-//             }
-//             var msg = document.getElementById("myMagnification").value;
-//             var demo2 = document.getElementById("demo2");
-//             if (mag == 1)
-//                 demo2.innerHTML = "5 micrometer";
-//             else if (mag == 2)
-//                 demo2.innerHTML = "10 micrometer";
-//             else if (mag == 3)
-//                 demo2.innerHTML = "50 micrometer";
-//             else(mag == 4)
-//             demo2.innerHTML = "100 micrometer";
-//         }
-// document.getElementById("mySpotSize").onchange = function () {
-//     var i = 0;
-//     var txt = '3. Click on the "Execute" button';
-//     var speed = 50;
-//     typeWriter();
-//     textToSpeech("click on the execute button");
-//     function typeWriter() {
-//         if (i < txt.length) {
-//             document.getElementById("demo3").innerHTML += txt.charAt(i);
-//             i++;
-//             setTimeout(typeWriter, speed);
-//         }
-//     }
-//     var sa = document.getElementById("mySpotSize");
-//     var ss = document.getElementById("demo3");
-//     if (sa == 1)
-//         ss.innerHTML = "29"
-//     else if (sa == 2)
-//         ss.innerHTML = "31";
-//     else if (sa == 3)
-//         ss.innerHTML = "35";
-// }
-// textToSpeech("select the type of experiment to be performed")
-// if (c == 1) {
-//     document.getElementById("btn1").style.background = '#A93226';
-//     document.getElementById("btn2").disabled = true;
-//     document.getElementById("btn3").disabled = true;
-//     document.getElementById("img1").src = "./images/im1.png";
-//     document.getElementById("img2").src = "./images/im2.png";
-//     document.getElementById("img3").src = "./images/im3.png";
-// }
-// if (c == 2) {
-//     document.getElementById("btn2").style.background = '#A93226';
-//     document.getElementById("btn1").disabled = true;
-//     document.getElementById("btn3").disabled = true;
-//     document.getElementById("img1").src = "./images/im1.png";
-//     document.getElementById("img2").src = "./images/im2.png";
-//     document.getElementById("img3").src = "./images/im3.png";
-// }
-// if (c == 3) {
-//     document.getElementById("btn3").style.background = '#A93226';
-//     document.getElementById("btn2").disabled = true;
-//     document.getElementById("btn1").disabled = true;
-//     document.getElementById("img1").src = "./images/im1.png";
-//     document.getElementById("img2").src = "./images/im2.png";
-//     document.getElementById("img3").src = "./images/im3.png";
-// }
-// }
-
-
-// function voltagefunction() {
-//     var img = document.createElement('img');
-//     document.getElementById("machine_image").src = "./gif/sem.gif";
-//     console.log(c);
-//     document.getElementById("task").disabled = false;
-//     var d = document.getElementById("myRange");
-//     var mag = document.getElementById("myMagnification");
-//     var ss = document.getElementById("mySpotSize");
-//     console.log(d.value);
-//     if ((c == 1)) {
-//         if (d.value == 1) {
-//             img.src = './images/v1.5.jpg';
-//             var e = document.getElementById("demo");
-//         } else if (d.value == 2) {
-//             img.src = './images/v1.10.jpg';
-//             var e = document.getElementById("demo");
-//         } else if (d.value == 3) {
-//             img.src = './images/v1.20.jpg';
-//             var e = document.getElementById("demo");
-//         }
-//     }
-//     if ((c == 2)) {
-//         if (mag.value == 0) {
-//             img.src = "./images/m1.1.jpg";
-//         } else if (mag.value == 1) {
-//             img.src = "./images/m1.1.jpg";
-//         } else if (mag.value == 2) {
-//             img.src = "./images/m1.1.jpg";
-//         } else if (mag.value == 3) {
-//             img.src = "./images/m1.1.jpg";
-//         } else if (mag.value == 4) {
-//             img.src = "./images/m1.1.jpg";
-//         }
-//     }
-//     if (c == 3) {
-//         console.log("c", c, ss.value);
-//         if (ss.value == 0) {
-//             img.src = "./images/s1.1.jpg";
-//         } else if (ss.value == 1) {
-//             img.src = "./images/s1.2.jpg";
-//         } else if (ss.value == 2) {
-//             img.src = "./images/s1.3.jpg";
-//         }
-//     }
-//     // function modal() {
-//     //     textToSpeech("the first image is material image at 5 kilo volts.");
-//     //     textToSpeech("the second image is at 10 kilo volts.");
-//     //     textToSpeech("the third image is at 20 kilo volts.")
-//     // }
-//     document.getElementById('image_display').innerHTML = "";
-//     document.getElementById('image_display').appendChild(img);
-// }
-// //text to speech
-
-// // list of languages is probably not loaded, wait for it
-// if (window.speechSynthesis.getVoices().length == 0) {
-//     window.speechSynthesis.addEventListener('voiceschanged', function() {
-//         textToSpeech(text);
-//     });
-// }
-
-// function textToSpeech(text) {
-//     // get all voices that browser offers
-//     var available_voices = window.speechSynthesis.getVoices();
-
-//     // this will hold an english voice
-//     var english_voice = '';
-
-//     // find voice by language locale "en-US"
-//     // if not then select the first voice
-//     for (var i = 0; i < available_voices.length; i++) {
-//         if (available_voices[i].lang === 'en-US') {
-//             english_voice = available_voices[i];
-//             break;
-//         }
-//     }
-//     if (english_voice === '')
-//         english_voice = available_voices[0];
-
-//     // new SpeechSynthesisUtterance object
-//     var utter = new SpeechSynthesisUtterance();
-//     utter.rate = 1;
-//     utter.pitch = 0.5;
-//     utter.text = text;
-//     utter.voice = english_voice;
-
-
-//     // speak
-//     window.speechSynthesis.speak(utter);
-// }
-
-// function task() {
-//     document.getElementById("container_exp").style.display = "none";
-//     document.getElementById("container_task").style.display = "block";
-// }
 /////////////////////////////Task JS//////////////////////////////
 var tWrapper = $("#toast-wrapper"),
     ti = 0;
@@ -618,18 +354,23 @@ function execute() {
     if (value == 3) {
         if (spotsize_range == 0) {
             document.getElementById("image_display").src = "./images/ss4.PNG"
+            document.getElementById('spotsizetext').innerHTML = '151p'
         }
         if (spotsize_range == 1) {
             document.getElementById("image_display").src = "./images/ss3.PNG"
+            document.getElementById('spotsizetext').innerHTML = '201p'
         }
         if (spotsize_range == 2) {
             document.getElementById("image_display").src = "./images/ss2.PNG"
+            document.getElementById('spotsizetext').innerHTML = '253p'
         }
         if (spotsize_range == 3) {
             document.getElementById("image_display").src = "./images/ss1.PNG"
+            document.getElementById('spotsizetext').innerHTML = '302p'
         }
         if (spotsize_range == 4) {
             document.getElementById("image_display").src = "./images/ss0.PNG"
+            document.getElementById('spotsizetext').innerHTML = '354p'
         }
     }
     /////////////////////////////////////////////////////////////
@@ -729,9 +470,21 @@ function workingdistancerange(workingdistancevalue){
         document.getElementById("workingdistancetext").innerHTML = "7.00mm" 
     }
 }
-// function spotsizerange(ssvalue){
-//     if(ssvalue == 0){
-//         document.getElementById("spotsizetext").innerHTML = "7.00mm"
-//     }
-// }
+function spotsizerange(ssvalue){
+    if(ssvalue == 0){
+        document.getElementById("spotsizetext").innerHTML = "151p"
+    }
+    if(ssvalue == 1){
+        document.getElementById('spotsizetext').innerHTML = "201p"
+    }
+    if(ssvalue == 2){
+        document.getElementById('spotsizetext').innerHTML = "253p"
+    }
+    if(ssvalue == 3){
+        document.getElementById('spotsizetext').innerHTML = "302p"
+    }
+    if(ssvalue == 4){
+        document.getElementById('spotsizetext').innerHTML = "354p"
+    }
+}
 
